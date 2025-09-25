10ece5250f62f61aa7823348a5b6605ad7b4cbab
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-09-25 11:23:21 -0300

    feat: mi primera clase
    👽👽👽👽
    Mi primera clase con Python
    ヾ(•ω•`)o (●'◡'●)

---
+++ clase01u2.py
@@ -0,0 +1,15 @@
+class Participante:
+    def __init__(self, rut: str, nombre: str, edad: int):
+        self._rut: str = rut
+        self._nombre: str = nombre
+        self._edad: int = edad
+
+    def presentarse(self) -> str:
+        return f"Hola mi nombre es {self._nombre} y mi edad es {self._edad}"
+    
+    def __str__(self):
+        return f"{self._rut} {self._nombre} {self._edad}"
+
+participante1 : Participante = Participante("19567387-K", "Felipe Villaroel", 24)
+
+print(participante1)
