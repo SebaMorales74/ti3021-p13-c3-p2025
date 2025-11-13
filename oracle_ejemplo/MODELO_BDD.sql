e68a6f6a9caea529d44e4cfa4e8f0c9a02aaa3d5
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-11-13 11:16:58 -0300

    clase 13-11-2025

---
+++ oracle_ejemplo/MODELO_BDD.sql
@@ -0,0 +1,34 @@
+/*
+-- Nombre
+-- Atributos: TIPO DE DATO (SQL) (CONSTRAINT)
+PERSONAS
+EMPLEADO
+DEPARTAMENTO
+*/
+
+CREATE TABLE
+    PERSONAS (
+        id INTEGER PRIMARY KEY,
+        rut VARCHAR(8),
+        nombres VARCHAR(64),
+        apellidos VARCHAR(64),
+        fecha_nacimiento DATE
+    )
+
+CREATE TABLE
+DEPARTAMENTO(
+    id INTEGER PRIMARY KEY,
+    nombre VARCHAR(32),
+    fecha_creacion DATE,
+    habilitado BOOLEAN
+)
+
+CREATE TABLE
+    EMPLEADO (
+        id INTEGER PRIMARY KEY,
+        sueldo INTEGER,
+        idPersona INTEGER NOT NULL,
+        idDepartamento INTEGER NOT NULL,
+        FOREIGN KEY (idPersona) REFERENCES PERSONAS(id),
+        FOREIGN KEY (idDepartamento) REFERENCES DEPARTAMENTO(id)
+    )
--- oracle_ejemplo/ejemplo_crud.py
+++ oracle_ejemplo/ejemplo_crud.py
@@ -7,26 +7,51 @@ username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
 
+
 def get_connection():
     return oracledb.connect(user=username, password=password, dsn=dsn)
 
 
-def create_table_personas():
-    query = (
-        "CREATE TABLE personas ("
-        "rut VARCHAR2(50) PRIMARY KEY,"
-        "nombres VARCHAR2(200),"
-        "apellidos VARCHAR2(200),"
-        "fecha_nacimiento DATE,"
-        "cod_area VARCHAR2(20),"
-        "numero_telefono VARCHAR2(50)"
-        ")"
-    )
+def create_schema():
 
-    try:
-        with get_connection() as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                print("Tabla 'personas' creada.")
-    except oracledb.DatabaseError as error:
-        print(f"No se pudo crear la tabla: {error}")
+    tables = [
+        (
+            "CREATE TABLE"
+            "PERSONAS ("
+            "id INTEGER PRIMARY KEY,"
+            "rut VARCHAR(8),"
+            "nombres VARCHAR(64),"
+            "apellidos VARCHAR(64),"
+            "fecha_nacimiento DATE"
+            ")"
+        ),
+        (
+            "CREATE TABLE"
+            "DEPARTAMENTO("
+            "id INTEGER PRIMARY KEY,"
+            "nombre VARCHAR(32),"
+            "fecha_creacion DATE,"
+            "habilitado BOOLEAN"
+            ")"
+        ),
+        (
+            "CREATE TABLE"
+            "EMPLEADO ("
+            "id INTEGER PRIMARY KEY,"
+            "sueldo INTEGER,"
+            "idPersona INTEGER NOT NULL,"
+            "idDepartamento INTEGER NOT NULL,"
+            "FOREIGN KEY (idPersona) REFERENCES PERSONAS(id),"
+            "FOREIGN KEY (idDepartamento) REFERENCES DEPARTAMENTO(id)"
+            ")"
+        )
+    ]
+
+    for query in tables:
+        try:
+            with get_connection() as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(query)
+                    print(f"Tabla creada \n {query}")
+        except oracledb.DatabaseError as error:
+            print(f"No se pudo crear la tabla: {error}")
