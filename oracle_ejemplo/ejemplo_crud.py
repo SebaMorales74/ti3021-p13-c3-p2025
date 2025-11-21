58b57a564709c47c743c02b1ea433092f08ae703
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-11-21 11:13:30 -0300

    feat: 21-11-2025

--- oracle_ejemplo/ejemplo_crud.py
+++ oracle_ejemplo/ejemplo_crud.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import oracledb
 import os
 from dotenv import load_dotenv
@@ -54,13 +55,13 @@ tables = [
 for query in tables:
     create_schema(query)
 
-from datetime import datetime
+# CREATE - Inserción de datos
 def create_persona(
-    id,
-    rut,
-    nombres,
-    apellidos,
-    fecha_nacimiento
+    id: int,
+    rut: str,
+    nombres: str,
+    apellidos: str,
+    fecha_nacimiento: str
 ):
     sql = (
         "INSERT INTO PERSONAS(id,rut,nombres,apellidos,fecha_nacimiento)"
@@ -75,6 +76,15 @@ def create_persona(
         "fecha_nacimiento": datetime.strptime(fecha_nacimiento, '%d-%m-%Y')
     }
 
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(sql, parametros)
+            connection.commit()
+            print("Inserción de datos correcta")
+    except oracledb.DatabaseError as error:
+        print(
+            f"No se pudo insertar el dato \n {error} \n {sql} \n {parametros}")
 
 def create_departamento(
     id,
@@ -84,7 +94,7 @@ def create_departamento(
     sql = (
         "INSERT INTO DEPARTAMENTO(id,nombre,fecha_creacion)"
         "VALUES (:id,:nombre,:fecha_creacion)"
-    ) 
+    )
 
     parametros = {
         "id": id,
@@ -92,6 +102,15 @@ def create_departamento(
         "fecha_creacion": datetime.strptime(fecha_creacion, '%d-%m-%Y')
     }
 
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(sql, parametros)
+            connection.commit()
+            print("Inserción de datos correcta")
+    except oracledb.DatabaseError as error:
+        print(
+            f"No se pudo insertar el dato \n {error} \n {sql} \n {parametros}")
 
 def create_empleado(
     id,
@@ -103,10 +122,122 @@ def create_empleado(
         "INSERT INTO EMPLEADO(id,sueldo,idPersona,idDepartamento)"
         "VALUES (:id,:sueldo,:idPersona,:idDepartamento)"
     )
-    
+
     parametros = {
         "id": id,
         "sueldo": sueldo,
         "idPersona": idPersona,
         "idDepartamento": idDepartamento
     }
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(sql, parametros)
+            connection.commit()
+            print("Inserción de datos correcta")
+    except oracledb.DatabaseError as error:
+        print(
+            f"No se pudo insertar el dato \n {error} \n {sql} \n {parametros}")
+
+# READ - Lectura de datos
+def read_personas():
+    sql = (
+        "SELECT * FROM PERSONAS"
+    )
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                print(sql)
+                resultados = cursor.execute(sql)
+                for fila in resultados:
+                    print(fila)
+    except oracledb.DatabaseError as error:
+        print(f"No se pudo ejecutar la query {error} \n {sql}")
+        
+def read_persona_by_id(id: int):
+    sql = (
+        "SELECT * FROM PERSONAS WHERE id = :id"
+    )
+    parametros = {"id" : id}
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                print(sql, parametros)
+                resultados = cursor.execute(sql, parametros)
+                if len(resultados) == 0:
+                    return print(f"No hay registros con el ID {id}")
+                for fila in resultados:
+                    print(fila)
+    except oracledb.DatabaseError as error:
+        print(f"No se pudo ejecutar la query {error} \n {sql} \n {parametros}")
+
+def read_departamentos():
+    sql = (
+        "SELECT * FROM DEPARTAMENTOS"
+    )
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                print(sql)
+                resultados = cursor.execute(sql)
+                for fila in resultados:
+                    print(fila)
+    except oracledb.DatabaseError as error:
+        print(f"No se pudo ejecutar la query {error} \n {sql}")
+
+def read_departamento_by_id(id: int):
+    sql = (
+        "SELECT * FROM DEPARTAMENTOS WHERE id = :id"
+    )
+    parametros = {"id" : id}
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                print(sql, parametros)
+                resultados = cursor.execute(sql, parametros)
+                if len(resultados) == 0:
+                    return print(f"No hay registros con el ID {id}")
+                for fila in resultados:
+                    print(fila)
+    except oracledb.DatabaseError as error:
+        print(f"No se pudo ejecutar la query {error} \n {sql} \n {parametros}")
+
+def read_empleados():
+    sql = (
+        "SELECT * FROM EMPLEADOS"
+    )
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                print(sql)
+                resultados = cursor.execute(sql)
+                for fila in resultados:
+                    print(fila)
+    except oracledb.DatabaseError as error:
+        print(f"No se pudo ejecutar la query {error} \n {sql}")
+
+def read_empleado_by_id(id: int):
+    sql = (
+        "SELECT * FROM EMPLEADOS WHERE id = :id"
+    )
+    parametros = {"id" : id}
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                print(sql, parametros)
+                resultados = cursor.execute(sql, parametros)
+                if len(resultados) == 0:
+                    return print(f"No hay registros con el ID {id}")
+                for fila in resultados:
+                    print(fila)
+    except oracledb.DatabaseError as error:
+        print(f"No se pudo ejecutar la query {error} \n {sql} \n {parametros}")
+
+# UPDATE - Actualización de datos
