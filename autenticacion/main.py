113ac558025efbd4b9d5c27c9f18d06acd2e0503
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-12-13 01:53:49 -0300

    Update main.py

--- autenticacion/main.py
+++ autenticacion/main.py
@@ -1,85 +1,107 @@
-# Conectarnos a la base de datos
+import bcrypt
+import requests
 import oracledb
-# Rescatar variables de entorno
 import os
 from dotenv import load_dotenv
-# Implementar hasheo de contraeñas
-import bcrypt
-# Importar el tipo de dato Opcional
 from typing import Optional
-# Implementar peticiones HTTP
-import requests
-# Importar liberia de fechas
 import datetime
-# Cargar las variables desde el archivo .env
+
 load_dotenv()
-# Rescatar las credenciales de conexion con Oracle
-username = os.getenv("ORACLE_USER")
-dsn = os.getenv("ORACLE_DSN")
-password = os.getenv("ORACLE_PASSWORD")
 
 class Database:
-    def __init__(self, username, password, dsn):
+    def __init__(self, username, dsn, password):
         self.username = username
-        self.password = password
         self.dsn = dsn
+        self.password = password
     def get_connection(self):
         return oracledb.connect(user=self.username, password=self.password, dsn=self.dsn)
     def create_all_tables(self):
-        pass
-    def query(self, sentence: str, parameters: Optional[dict] = None):
-        print(f"Ejecutando query:\n{sentence}\nParametros:\n{parameters}")
+        tables = [
+            (
+                "CREATE TABLE USERS("
+                "id INTEGER PRIMARY KEY,"
+                "username VARCHAR(32) UNIQUE,"
+                "password VARCHAR(128)"
+                ")"
+            )
+        ]
+
+        for table in tables:
+            self.query(table)
+
+    def query(self, sql: str, parameters: Optional[dict] = None):
         try:
-            with self.get_connection() as connection:
-                with connection.cursor() as cursor:
-                    resultado = cursor.execute(sentence, parameters)
-                    return resultado
-                connection.commit()
+            with self.get_connection() as conn:
+                with conn.cursor() as cur:
+                    ejecucion = cur.execute(sql, parameters)
+                    if sql.startswith("SELECT"):
+                        resultado = []
+                        for fila in ejecucion:
+                            resultado.append(fila)
+                        return resultado
+                conn.commit()
         except oracledb.DatabaseError as error:
-            print(f"Hubo un error con al base de datos:\n{error}")
+            print(error)
 
-
-# Generar autenticacion
 class Auth:
     @staticmethod
-    def register(db: Database, username: str, password: str):
+    def login(db: Database, username: str, password: str):
+        password = password.encode("UTF-8")
+
+        resultado = db.query(
+            sql= "SELECT * FROM USERS WHERE username = :username",
+            parameters={"username":username}
+        )
+
+        if len(resultado) < 0:
+            return print("No hay coincidencias")
+        
+        hashed_password = bytes.fromhex(resultado[0][2])
+
+        if bcrypt.checkpw(password, hashed_password):
+            return print("Logeado correctamente")
+        else:
+            return print("Contraseña incorrecta")
+
+    @staticmethod
+    def register(db: Database, id: int, username: str, password: str):
+        print("registrando usuario")
+        password = password.encode("UTF-8")
         salt = bcrypt.gensalt(12)
-        hashed_password = bcrypt.hashpw(password,salt)
+        hash_password = bcrypt.hashpw(password,salt)
+
         usuario = {
-            "id": 1,
+            "id": id,
             "username": username,
-            "password": hashed_password
+            "password": hash_password
         }
 
         db.query(
-            "INSERT INTO USERS(id,username,password) VALUES (:id,:username:password)",
-            usuario
+            sql= "INSERT INTO USERS(id,username,password) VALUES (:id, :username, :password)",
+            parameters=usuario
         )
-    @staticmethod
-    def login(db: Database, username: str, password: str) -> bool:
-        resultado = db.query(
-            "SELECT * FROM USERS WHERE username = :username",
-            {"username" : username}
-        )
-
-        for usuario in resultado:
-            password_user = usuario[2]
-            return bcrypt.checkpw(password, password_user)
+        print("usuario registrado con exito")
 
 class Finance:
     def __init__(self, base_url: str = "https://mindicador.cl/api"):
         self.base_url = base_url
-    def get_indicator(self, indicator: str = None, fecha:str=None):
-        if not indicator:
-            return print("Indicador faltante")
-        if not fecha:
-            year = datetime.datetime.now().year
-            month = datetime.datetime.now().month
-            day = datetime.datetime.now().day
-            fecha = f"{day}-{month}-{year}"
-        url = f"{self.base_url}/{indicator}/{fecha}"
-        data = requests.get(url=url).json()
-        print(data['serie'][0]['valor']) 
+    def get_indicator(self, indicator: str, fecha: str = None) -> float:
+        try:
+            if not fecha:
+                dd = datetime.datetime.now().day
+                mm = datetime.datetime.now().month
+                yyyy = datetime.datetime.now().year
+                fecha = f"{dd}-{mm}-{yyyy}"
+            url = f"{self.base_url}/{indicator}/{fecha}"
+            respuesta = requests.get(url).json()
+            return respuesta["serie"][0]["valor"]
+        except:
+            print("Hubo un error con la solicitud")
+    def get_usd(self, fecha: str = None):
+        valor = self.get_indicator("dolar", fecha)
+        print(f"El valor del dolar en CLP es: {valor}")
+    def get_eur(self, fecha: str = None):
+        self.get_indicator("euro", fecha)
     def get_uf(self, fecha: str = None):
         self.get_indicator("uf", fecha)
     def get_ivp(self, fecha: str = None):
@@ -88,10 +110,12 @@ class Finance:
         self.get_indicator("ipc", fecha)
     def get_utm(self, fecha: str = None):
         self.get_indicator("utm", fecha)
-    def get_usd(self, fecha: str = None):
-        self.get_indicator("dolar", fecha)
-    def get_eur(self, fecha: str = None):
-        self.get_indicator("euro", fecha)
 
 if __name__ == "__main__":
-    indicadores = Finance()
+    db = Database(
+        username=os.getenv("ORACLE_USER"),
+        password=os.getenv("ORACLE_PASSWORD"),
+        dsn=os.getenv("ORACLE_DSN")
+    )
+
+    Auth.login(db,"soyelseba","alskjflsakf")
